ea7141cf5b3d459e3960bba49139e6e8d65a20b0
Keller Maciel <bkelly.sm@gmail.com>
2023-01-28 20:33:39 -0300

    Cria componente header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HexComponent } from './hex/hex.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HexComponent
+    HexComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
